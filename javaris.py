0df66b5014a08cbdcda07c671bdb7ecd2b79cabf
Ananya015 <jainananya78@gmail.com>
2020-08-21 01:19:54 +0530

    add takeCommand function

--- javaris.py
+++ javaris.py
@@ -12,6 +12,7 @@ engine.setProperty('voice', voices[1].id)
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
+
 def wishMe():
     hour = int(datetime.datetime.now().hour)
     if hour>=0 and hour<12:
@@ -22,6 +23,23 @@ def wishMe():
         speak("Good Evening")
 
     speak("I am jarvis, please tell me how can I help you")
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening.....")
+        r.pause_threshold =1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing....")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}")
+    except Exception as e:
+        print("Say again please")
+        return "None"
+    return query
+
 
 if __name__ == '__main__':
     speak("hey ! welcome. This is Ananya")
