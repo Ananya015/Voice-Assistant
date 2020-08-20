10424281b10140dd80a5804b464dd4d020fb4bb4
Ananya015 <jainananya78@gmail.com>
2020-08-21 01:06:16 +0530

    import module

--- javaris.py
+++ javaris.py
@@ -1,9 +1,11 @@
 import pyttsx3
 import datetime
+import speech_recognition as sr
+
 engine=pyttsx3.init('sapi5')
 voices=engine.getProperty('voices')
 
-# print(voices[0].id)
+
 engine.setProperty('voice', voices[1].id)
 
 
